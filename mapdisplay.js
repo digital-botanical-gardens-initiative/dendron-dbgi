9fd45281dd2b2ed0b2686c8cd543c26e539b5946
Edouard Bruelhart <edouard.bruelhart@unifr.ch>
2023-01-03 15:11:52 +0100

    up drone map

--- mapdisplay.js
+++ mapdisplay.js
@@ -1,12 +1,19 @@
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([0, 0]),
-      zoom: 2
+var view = new View({
+  center: [-9101767, 2822912],
+  zoom: 14
+});
+
+var map = new Map({
+  controls: defaultControls().extend([
+    new FullScreen({
+      source: 'fullscreen'
     })
-  });
+  ]),
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  target: 'map',
+  view: view
+});
